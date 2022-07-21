69b1fccaaccc2c46e848a83ae07d1e92629580a7
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-07-21 14:58:59 +0900

    배너 slider

---
+++ swiper/app.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // 하단 페이지 / If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
